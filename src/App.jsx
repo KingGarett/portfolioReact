ba11e56c5be179f8fa03071fe45dd596044b77e4
kinggarett <carlosjavier094@gmail.com>
2023-02-20 01:22:10 -0300

    adding presentation and create before,after in the main imagen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Background from "./assets/components/Background"
+import Presentation from './assets/components/Presentation'
 
 function App() {
 
   return (
     <div className="min-h-screen bg-slate-900">
+      <Presentation/>
       <Background/>
     </div>
   )
--- src/assets/components/Background.jsx
+++ src/assets/components/Background.jsx
@@ -1,41 +1,46 @@
-import img from '../image/street2.png'
+import athom from '../image/athom.png'
+import design from '../image/design-is-mine-3.png'
+
 const Background = () => {
   return (
-    <div className="relative z-10 w-full h-screen overflow-hidden">
-        <div className="relative flex">
-            <span style={{'--i':'11'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'12'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'24'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'10'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'14'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'23'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'18'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'16'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'19'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'20'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'22'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'25'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'18'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'21'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'13'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'26'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'17'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'37'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'28'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'11'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'17'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'35'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'10'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'14'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'23'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'18'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'16'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'19'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'35'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'22'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'17'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            <span style={{'--i':'28'}} className="relative z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
-            {/* <img className='absolute z-0' src={img}/> */}
+    <div className="z-10 w-full h-screen overflow-hidden ">
+        <div className="flex ">
+            <span style={{'--i':'11'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'12'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'24'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'10'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'14'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'23'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'18'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'16'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'19'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'20'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'22'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'25'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'18'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'21'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'13'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'26'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'17'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'37'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'28'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'11'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'17'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'35'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'10'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'14'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'23'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'18'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'16'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'19'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'35'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'22'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'17'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <span style={{'--i':'28'}} className=" z-10 sm:w-[3vw] w-[1vw] sm:h-[3vh] md:h-[4vh] h-[1vh] mx-1 rounded-full stars-move sphere-shadow bg-sky-100"></span>
+            <div className='absolute before:content-[""] before:absolute before:h-16  before:w-full  before:bg-gradient-to-b from-slate-900 to-transparent before:top-0 before:z-0 after:content-[""] after:absolute after:h-24  after:bottom-0 after:w-full  after:bg-gradient-to-t  after:z-50 z-20'>
+              <img className='z-30 ' src={design}/>
+              <img className='absolute left-[10%] top-10 z-20 w-1/3 lg:w-2/5 rotate-athom' src={athom}/>
+            </div>
         </div>
     </div>
   )
